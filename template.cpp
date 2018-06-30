f16d867383d9649a23da90fd7e836e6da3fb0f7a
ああああああああ <azurite@azurite.local>
2018-06-30 20:56:33 +0900

    template test

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <stack>
+#include <queue>
+#include <cstdio>
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+#include <string>
+ 
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;i++)
+#define Rep(i,n0,n) for(int i=n0;i<n;i++)
+#define Com(i,j,n) for(int i=0;i<n-1;i++)for(int j=i+1;j<n;j++)
+#define INF (1<<29)
+
+int main (){
+
+	return 0;
+}
