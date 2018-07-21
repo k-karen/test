1c8414785af8ec935cf8d8842ad5dc89c2ab1ada
Your Name <birth.colors@gmail.com>
2018-07-22 00:01:13 +0900

    ちょっとまとめて

--- ABC103D.cpp
+++ ABC103D.cpp
@@ -14,32 +14,22 @@ using namespace std;
 #define INF (1<<29)
 
 int main (){
-	int NN,MM;
+	int NN,MM,tempa,tempb,temp;
 	cin >> NN >> MM;
-	int tempa,tempb;
-	int temp;
 	pair<int,int> temppair;
 	vector <pair< int , int > > cuts;
 	rep(i,MM){
 		cin >> tempa >> tempb;
-		if(tempa>tempb){
-			temp = tempa;
-			tempa = tempb;
-			tempb = temp;
-		}
-		temppair = make_pair(tempa,tempb);
-		cuts.push_back(temppair);
+		if(tempa>tempb)	{temp = tempa;tempa = tempb;tempb = temp;}
+		cuts.push_back(make_pair(tempa,tempb));
 	}
 	sort(cuts.begin(),cuts.end());
-
 	int ff = cuts[0].first;
 	int fb = cuts[0].second;
-	int ans = 0;
+	int ans = 1;
 	rep(i,MM){
-		if(cuts[i].first == ff)	{
-			continue;
-		}
-		if( fb <= cuts[i].first ){
+		if(cuts[i].first == ff)	continue;
+		if ( fb <= cuts[i].first ){
 			ff = cuts[i].first;
 			fb = cuts[i].second;
 			ans ++;
@@ -48,8 +38,6 @@ int main (){
 			fb = cuts[i].second;
 		}
 	}
-	cout << ans+1 << endl;
-
+	cout << ans << endl;
 	return 0;
-
 }
