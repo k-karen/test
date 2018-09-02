3d91f8bf5bfaa561722b774a84c07510d55c9a20
Kujo Karen <birth.colors@gmail.com>
2018-09-02 09:07:42 +0900

    ローカル環境のgcc新しくしたのでテンプレートを変えた

--- template.cpp
+++ template.cpp
@@ -1,19 +1,17 @@
-#include <stack>
-#include <queue>
-#include <cstdio>
-#include <iostream>
-#include <stdio.h>
-#include <algorithm>
-#include <math.h>
-#include <string>
+#include <bits/stdc++.h>
  
 using namespace std;
 #define rep(i,n) for(int i=0;i<n;i++)
 #define Rep(i,n0,n) for(int i=n0;i<n;i++)
 #define Com(i,j,n) for(int i=0;i<n-1;i++)for(int j=i+1;j<n;j++)
-#define INF (1<<29)
+const int INF =100100100;
+const int MOD = (int)1e9+7;
+const double EPS = 1e-9;
+typedef long long ll;
+typedef long li;
+#define pb push_back 
+#define mp make_pair
 
-int main (){
-
+signed main (){	
 	return 0;
 }
